ce5dfa6f8d4f20dc6e5276503fb277e1899ba0d5
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2021-05-16 11:35:30 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -74,14 +74,14 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-𝙷𝙴𝚈,my name is Haguromo Otsusuki/n
-𝙸'𝙰𝙼 𝙰𝙽 𝙰𝙽𝙸𝙼𝙴 𝚃𝙷𝙴𝙼𝙴𝙳 𝙶𝚁𝙾𝚄𝙿 𝙼𝙰𝙽𝙰𝙶𝙸𝙽𝙶 𝙱𝙾𝚃! BY SATYA AND EMCEE DEVS 𝙲𝙻𝙸𝙲𝙺` *📚𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂* `𝙱𝚄𝚃𝚃𝙾𝙽 𝙱𝙴𝙻𝙾𝚆 𝚃𝙾 𝙴𝚇𝙿𝙻𝙾𝚁𝙴 𝙼𝙾𝚁𝙴 𝙰𝙱𝙾𝚄𝚃 𝙼𝙴 𝙰𝙽𝙳 𝙼𝚈 𝙵𝚄𝙽𝙲𝚃𝙸𝙾𝙽𝚂 [.](https://telegra.ph/file/36c57221ea00d88fbcd89.jpg)
+𝙷𝙴𝚈,my name is TIANA BOT/n
+𝙸'𝙰𝙼 𝙰𝙽 𝙶𝚁𝙾𝚄𝙿 𝙼𝙰𝙽𝙰𝙶𝙸𝙽𝙶 𝙱𝙾𝚃!` *📚𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂* `𝙱𝚄𝚃𝚃𝙾𝙽 𝙱𝙴𝙻𝙾𝚆 𝚃𝙾 𝙴𝚇𝙿𝙻𝙾𝚁𝙴 𝙼𝙾𝚁𝙴 𝙰𝙱𝙾𝚄𝚃 𝙼𝙴 𝙰𝙽𝙳 𝙼𝚈 𝙵𝚄𝙽𝙲𝚃𝙸𝙾𝙽𝚂 [.](https://telegra.ph/file/36c57221ea00d88fbcd89.jpg)
 """
 
 buttons = [
     [
         InlineKeyboardButton(
-            text="➕️ ADD HAGUROMO TO YOUR GROUP ➕️", url="t.me/rikudo_senin_bot?startgroup=true"),
+            text="➕️ ADD HAGUROMO TO YOUR GROUP ➕️", url="t.me/Tiana_Prince_bot?startgroup=true"),
     ],
     [
         InlineKeyboardButton(text="ℹ️ABOUT", callback_data="masha_"),
@@ -89,12 +89,12 @@ buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="✨Owner✨", url="http://t.me/satyanandatripathi"),
+            text="✨Owner✨", url="http://t.me/PRINCE_3011"),
         InlineKeyboardButton(
-            text="👥SUPPORT👥", url="https://t.me/domin8orsupport"),
+            text="👥SUPPORT👥", url="https://t.me/PRINCE_BOTS_SUPPORT"),
     ],
     [  
-        InlineKeyboardButton(text="Thanks to", url="https://t.me/emcee_support"),
+        InlineKeyboardButton(text="Thanks to", url="https://t.me/PRINCE_NETWORK"),
     ], 
     
 ]
@@ -102,10 +102,10 @@ buttons = [
 
 
 HELP_STRINGS = """
-༆*Rikudo Senin comes with many special features in it*༆
+⚜️ TIANO BOT WITH NEW FEATURES ⚜️
 ꧁*CHECK ALL BUTTON BELOW TO EXPLORE EVERY COMMANDS OF EMCEE*꧂
 𖣘 *All commands can either be used with* `/` *or* `!`.
-𖣘 *If you facing any issue or find any bugs in any command then you can report it in @Emcee_Support* [.](https://telegra.ph/file/36c57221ea00d88fbcd89.jpg)
+𖣘 *If you facing any issue or find any bugs in any command then you can report it in @PRINCE_BOTS_SUPPORT* [.](https://telegra.ph/file/36c57221ea00d88fbcd89.jpg)
 """
 
 
@@ -354,7 +354,7 @@ def Masha_about_callback(update: Update, context: CallbackContext):
     query = update.callback_query
     if query.data == "masha_":
         query.message.edit_text(
-            text=""" ℹ️ I'm *Haguromo Otsusuki*, a powerful group management bot built to help you manage your group easily.
+            text=""" ℹ️ I'm *TIANA BOT*, a powerful group management bot built to help you manage your group easily.
                  ❍ I can restrict users.
                  ❍ I can greet users with customizable welcome messages and even set a group's rules.
                  ❍ I have an advanced anti-flood system.
@@ -362,7 +362,7 @@ def Masha_about_callback(update: Update, context: CallbackContext):
                  ❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
                  ❍ I check for admins' permissions before executing any command and more stuffs
                  \n_Emcee's licensed under the GNU General Public License v3.0_
-                 Here is the [💾Repository](https://www.github.com/satyanandatripathi/emcee).
+                 Here is the [💾Repository](https://www.github.com/prince301102/tiana-2.0).
                  If you have any question about Rikudo senin, let us know at @satyanandatripathi.""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
@@ -389,8 +389,8 @@ def Source_about_callback(update: Update, context: CallbackContext):
     query = update.callback_query
     if query.data == "source_":
         query.message.edit_text(
-            text=""" Hi..🤗 I'm *Haguromo Otsusuki*
-                 \nHere is the [Source Code](https://GitHub.com/satyanandatripathi/emcee) .""",
+            text=""" Hi..🤗 I'm *TIANA BOT*
+                 \nHere is the [Source Code](https://GitHub.com/prince301102/tiana-2.0) .""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=False,
             reply_markup=InlineKeyboardMarkup(
