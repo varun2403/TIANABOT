c0935a53c7764d6c9e8b4c2d4cec70e39844d891
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2021-05-19 12:27:58 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -366,9 +366,10 @@ def Masha_about_callback(update: Update, context: CallbackContext):
                  ❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
                  ❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
                  ❍ I check for admins' permissions before executing any command and more stuffs
-                 \n_Emcee's licensed under the GNU General Public License v3.0_
+                 \n_Tiana's licensed under the GNU General Public License v3.0_
                  Here is the [💾Repository](https://www.github.com/prince301102/tiana-2.0).
-                 If you have any question about Tiana, let us know at @Prince_3011.""",
+                 If you have any question about Tiana let us know at @Prince_3011.
+                 """,
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
